dcf42b79c947c779b26777eaccad19e1ce95e29e
Scott Covert <scott.b.covert@gmail.com>
2025-01-03 15:23:28 -0500

    Pulled in local.js to repo

---
+++ app/config/env/local.js
@@ -0,0 +1,6 @@
+/* Set "ENV" Vars */
+var FILEPICKER_API_KEY='',
+	FIREBASE_URL='https://tamcovertwedding.firebaseio.com/',
+	GOOGLE_MAPS_API_KEY='AIzaSyAwaX5Ab980nLTJHFxl7pxwe5UbHlKbq8Q',
+	WEDDING_HASHTAG='',
+	INSTAGRAM_CLIENT_ID='';
